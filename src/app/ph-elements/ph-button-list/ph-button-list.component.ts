db85395f5ec5cf6bd5b53d8650625b5a955c0d42
Lucas Moesch <lucasmoesch@live.de>
2022-01-30 14:15:13 +0100

    WIP

--- src/app/ph-elements/ph-button-list/ph-button-list.component.ts
+++ src/app/ph-elements/ph-button-list/ph-button-list.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface PhButtonListOption {
+  label: string;
+  func: any;
+} 
+
 @Component({
   selector: 'app-ph-button-list',
   templateUrl: './ph-button-list.component.html',
   styleUrls: ['./ph-button-list.component.scss']
 })
 export class PhButtonListComponent implements OnInit {
+
+  public optionList: PhButtonListOption[] = [
+    {label: "Login A", func: () => {}},
+    {label: "Login B", func: () => {}}
+  ];
 
   constructor() { }
 
