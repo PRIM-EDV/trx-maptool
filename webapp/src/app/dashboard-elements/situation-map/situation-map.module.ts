4f59087b14504cdaeb9fa6576130845d1add354a
Lucas Mösch <lucasmoesch@live.de>
2022-08-24 21:08:32 +0200

    Added rld-map submodule

---
+++ webapp/src/app/dashboard-elements/situation-map/situation-map.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SituationMapComponent } from './situation-map.component';
+
+
+
+@NgModule({
+  declarations: [
+    SituationMapComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SituationMapModule { }
