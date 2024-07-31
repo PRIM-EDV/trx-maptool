fe7d265ae81d23684cf78ce957af188ff508b14a
Lucas Moesch <lucasmoesch@live.de>
2024-07-31 21:16:04 +0200

    Finalized design of error overlay

--- webapp/src/app/overlay/overlay.component.ts
+++ webapp/src/app/overlay/overlay.component.ts
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
 })
 export class OverlayComponent {
     header: string = 'System Power Error';
-    message: string = 'A critical power loss within the section has been detected. The system is shutting down to prevent data loss and hardware damage. Further information on the problem is collected and will be sent to the technical support section and system administrator. Please remain patient and wait for the problem beeing solved.';
+    message: string = 'A critical power loss within the section has been detected. The system is shutting down to prevent data loss and hardware damage. Further information on the problem is collected and will be sent to the technical support section and system administrator. Please remain patient and wait for the problem beeing resolved. ';
 }
