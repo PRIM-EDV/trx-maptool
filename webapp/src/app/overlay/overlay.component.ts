c1314d0c331711e644a1060104507531cfa5cc24
Lucas Moesch <lucasmoesch@live.de>
2024-07-31 21:52:14 +0200

    Added lsx protocol

--- webapp/src/app/overlay/overlay.component.ts
+++ webapp/src/app/overlay/overlay.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-overlay',
@@ -8,6 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './overlay.component.scss'
 })
 export class OverlayComponent {
+
+    @HostBinding('style.display') public display: string = 'none';
+    
     header: string = 'System Power Error';
     message: string = 'A critical power loss within the section has been detected. The system is shutting down to prevent data loss and hardware damage. Further information on the problem is collected and will be sent to the technical support section and system administrator. Please remain patient and wait for the problem beeing resolved. ';
+
+    public open() {
+        this.display = 'unset';
+    }
+
+    public close() {
+        this.display = 'none';
+    }
 }
