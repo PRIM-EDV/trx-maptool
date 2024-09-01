46312a7e4867cd1cc4debe7f4a3548b4c6ad3256
Lucas Mösch <lucasmoesch@live.de>
2024-09-01 20:29:09 +0200

    Added PowerState handle for offline overlay

--- webapp/src/app/overlay/overlay.component.ts
+++ webapp/src/app/overlay/overlay.component.ts
@@ -1,4 +1,7 @@
 import { Component, HostBinding } from '@angular/core';
+import { LsxBackendService } from '../backend/lsx.backend.service';
+import { Request } from 'proto/lsx/lsx';    
+import { PowerDevice, PowerState, SetDevicePowerState_Request } from 'proto/lsx/lsx.power';
 
 @Component({
   selector: 'app-overlay',
@@ -14,6 +17,10 @@ export class OverlayComponent {
     header: string = 'System Power Error';
     message: string = 'A critical power loss within the section has been detected. The system is shutting down to prevent data loss and hardware damage. Further information on the problem is collected and will be sent to the technical support section and system administrator. Please remain patient and wait for the problem beeing resolved. ';
 
+    constructor(private readonly lsx: LsxBackendService) {
+        lsx.onRequest.subscribe(this.handleRequest.bind(this));
+    }
+    
     public open() {
         this.display = 'unset';
     }
@@ -21,4 +28,24 @@ export class OverlayComponent {
     public close() {
         this.display = 'none';
     }
+
+    private handleRequest(e: {id: string, request: Request}) {
+        // if(e.request.setMapEntity) {
+        //     this.onSetMapEntity.next(e.request.setMapEntity.entity!);
+        // }
+        if (e.request.setDevicePowerState) {
+            this.handleSetDevicePowerStateRequest(e.request.setDevicePowerState);
+        }
+    }  
+    
+    private handleSetDevicePowerStateRequest(request: SetDevicePowerState_Request) {
+        if (request.device == PowerDevice.DEVICE_CIC_MAPTOOL) {
+            if (request.state == PowerState.POWER_STATE_UNPOWERED) {
+                this.open();
+            } else {
+                this.close();
+            }
+        }
+    }
+    
 }
